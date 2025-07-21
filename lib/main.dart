d650d1272197357c25f760cce8349b55312a7574
naina verma <nainaverma71515@gmail.com>
2025-07-21 20:38:48 +0530

    assignment

--- lib/main.dart
+++ lib/main.dart
@@ -123,6 +123,10 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
+        floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
