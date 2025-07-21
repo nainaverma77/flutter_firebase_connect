8e2e27e61c74b5730a35bf02f6b5681b234a1a0c
naina verma <nainaverma71515@gmail.com>
2025-07-21 20:40:08 +0530

    assignment firebase

--- lib/main.dart
+++ lib/main.dart
@@ -123,10 +123,6 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-        floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
